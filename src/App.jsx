97b9dc14301e1c3ac4f3131eb6813aeedfad1130
EdrickMuthamia <edrickmuthamia05@gmail.com>
2025-09-27 10:37:54 +0300

    feat: Implement main application structure in App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "./contexts/ThemeContext";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import About from "./pages/About";
 import Products from "./pages/Products";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
@@ -17,6 +18,7 @@ export default function App() {
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
             <Route path="/products" element={<Products />} />
             <Route path="/services" element={<Services />} />
             <Route path="/contact" element={<Contact />} />
